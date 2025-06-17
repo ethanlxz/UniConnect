129fc986ccf46bfb34ee95b4732c9bdf6d738189
rael1022 <lawys1022@gmail.com>
2025-06-18 07:40:42 +0800

    fix photo2

--- Backend/UniConnect/UniConnect/urls.py
+++ Backend/UniConnect/UniConnect/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -29,3 +31,6 @@ urlpatterns = [
     path('classing/', include('classing.urls')),
     path('grouping/', include('grouping.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
