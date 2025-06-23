04f20beabae482659e871c36ba23e3c573b508a7
pomelocheah <138595462+pomelocheah@users.noreply.github.com>
2025-06-23 21:26:25 +0800

    remove dbsqlite3 and media in git

--- Backend/UniConnect/remove_pycache_git.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-for root, dirs, files in os.walk("."):
-    if "__pycache__" in dirs:
-        pycache_path = os.path.join(root, "__pycache__")
-        subprocess.run(["git", "rm", "-r", "--cached", pycache_path])
